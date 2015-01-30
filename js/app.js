e6d2b27806ad5b7d1224251dba4e1d9dfd80adac
Javier Abadia <javier.abadia@esri.es>
2015-01-30 12:30:02 +0100

    sorted collection

--- js/app.js
+++ js/app.js
@@ -11,7 +11,8 @@ ContactManager.on("start", function()
 	var contacts = new ContactManager.ContactCollection([
 	{	firstName: 'Bob',		lastName: 'Nolan',		phoneNumber: '555-1111'	},
 	{	firstName: 'Alice',		lastName: 'WhoTheFuck',	phoneNumber: '555-2222' },
-	{	firstName: 'Charlie',	lastName: 'Campbell',	phoneNumber: '555-3333' }
+	{	firstName: 'Charlie',	lastName: 'Campbell',	phoneNumber: '555-3333' },
+	{	firstName: 'Alice',		lastName: 'The Best',	phoneNumber: '555-3333' }
 	]);
 
 	console.log(contacts);
--- js/contact.model.js
+++ js/contact.model.js
@@ -11,5 +11,9 @@ ContactManager.Contact = Backbone.Model.extend({
 ContactManager.ContactCollection = Backbone.Collection.extend({
 
 	model: ContactManager.Contact,
+	comparator: function(a,b) { 
+		return a.get('firstName')+ " " + a.get('lastName') >  b.get('firstName')+ " " + b.get('lastName');
+	}
+	//'firstName',
 
 });
