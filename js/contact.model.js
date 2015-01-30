8537b7edfa511ad1bc1c55200d2cdd7a475ae7fd
Javier Abadia <javier.abadia@esri.es>
2015-01-30 12:46:03 +0100

    contacts in-memory initialization

--- js/contact.model.js
+++ js/contact.model.js
@@ -18,5 +18,33 @@ ContactManager.module("Entities", function(Entities,ContactManager,Backbone,Mari
 
 	});
 
+
+	var _contacts;
+
+	var _initalizeContacts = function()
+	{
+		_contacts = new Entities.ContactCollection([
+		{	id: 1, firstName: 'Bob',		lastName: 'Nolan',		phoneNumber: '555-1111'	},
+		{	id: 2, firstName: 'Alice',		lastName: 'WhoTheFuck',	phoneNumber: '555-2222' },
+		{	id: 3, firstName: 'Charlie',	lastName: 'Campbell',	phoneNumber: '555-3333' },
+		{	id: 4, firstName: 'Alice',		lastName: 'The Best',	phoneNumber: '555-3333' }
+		]);
+	};
+
+	var API = 
+	{
+		getContactEntities: function()
+		{
+			if(_contacts === undefined)
+				_initalizeContacts();
+
+			return _contacts;
+		}
+	};
+
+	ContactManager.reqres.setHandler('contact:entities', function()
+	{
+		return API.getContactEntities();
+	});
 });
 
