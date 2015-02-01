adee63287a850dd4a2aad9142d3901005073e447
Javier Abadia <javi.abadia@gmail.com>
2015-02-01 23:47:06 +0100

    events

--- js/list.view.js
+++ js/list.view.js
@@ -1,8 +1,24 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _)
 {
 	List.Contact = Marionette.ItemView.extend({
-		template: '#contact-list-item',
 		tagName: 'tr',
+		template: '#contact-list-item',
+
+		events: {
+			"click": "highlightName",
+			"click td": "alertName",
+		},
+
+		highlightName: function(e)
+		{
+			e.preventDefault();
+			this.$el.toggleClass('warning');
+		},
+
+		alertName: function(e)
+		{
+			alert($(e.target).text());
+		}
 	});
 
 	List.Contacts = Marionette.CompositeView.extend({
