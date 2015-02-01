76f743b048fc78ae5b6b678ad96448b5b9fc6c7b
Javier Abadia <javi.abadia@gmail.com>
2015-02-01 21:21:22 +0100

    refactorizando en “modulos”

---
+++ js/course.model.js
@@ -0,0 +1,32 @@
+
+//https://api.coursera.org/api/catalog.v1/courses
+
+/*
+
+ContactManager.module("Entities", function(Entities,ContactManager,Backbone,Marionette,$,_)
+{
+	Entities.Course = Backbone.Model.extend({
+
+	});
+
+	Entities.Courses = Backbone.Collection.extend({
+
+		model: Entities.Course,
+		urL: 'https://api.coursera.org/api/catalog.v1/courses',
+
+	});
+
+	var _courses;
+
+	var _initializeCourses = function()
+	{
+
+	}
+
+	var API = 
+	{
+
+	}
+});
+
+*/
