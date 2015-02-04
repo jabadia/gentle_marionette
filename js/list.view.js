7f32470ccb69386bd4188e28cb4809f450642155
Javier Abadia <javier.abadia@esri.es>
2015-02-04 15:25:54 +0100

    controller and view in a table

--- js/list.view.js
+++ js/list.view.js
@@ -2,11 +2,32 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
 {
 	List.Contact = Marionette.ItemView.extend({
 		template: '#contact-list-item',
-		tagName: 'li',
+		tagName: 'tr',
+
+		events: {
+			"click button.js-delete": "deleteClicked",
+		},
+
+		deleteClicked: function(e)
+		{
+			e.stopPropagation();
+			this.trigger("contact:delete", this.model);
+		},
+
+		remove: function()
+		{
+			var self = this;
+			this.$el.fadeOut(function() {
+				Marionette.ItemView.prototype.remove.call(self);
+			});
+		}
 	});
 
-	List.Contacts = Marionette.CollectionView.extend({
-		tagName: 'ul',
+	List.Contacts = Marionette.CompositeView.extend({
+		tagName: 'table',
+		className: 'table table-hover',
+		template: '#contact-list',
 		childView: List.Contact,
+		childViewContainer: "tbody",
 	});
 })
